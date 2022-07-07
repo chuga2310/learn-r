82390b412286fb6be447c96e250bc6e600824791
Nguyen Dinh Khoi <dinhkhoi997@gmail.com>
2022-07-07 13:37:21 +0700

    fix contract

--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,13 @@
 #[test]
     fn works_with_add_new_and_sell() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
+        
+        let pen_id = "1234";
+        let owner = "aura1csk4psx5gz0l7c9u65289pwecntpwk9vf0c2xv";
 
-        let msg = ExecuteMsg::mint {
-            id: "lily_id".to_string(),
-            name: "lily".to_string(),
-            amount: 100,
-            price: 100,
+        let msg = ExecuteMsg::Mint {
+            id: pen_id.to_string(),
+            owner : owner.to_string()
         };
         let info = mock_info("creator", &coins(1000, "earth"));
         // we can just call .unwrap() to assert this was a success
@@ -17,47 +18,17 @@
             deps.as_ref(),
             mock_env(),
             QueryMsg::GetPen {
-                id: "lily_id".to_string(),
+                id: pen_id.to_string(),
             },
         )
         .unwrap();
         let pen = Pen {
-            id: "lily_id".to_string(),
-            name: "lily".to_string(),
-            amount: 100,
-            price: 100,
+            id: pen_id.to_string(),
+            owner : owner.to_string()
         };
         let expected = PenInfoResponse {
             pen: Some(pen),
         };
         let value: PenInfoResponse = from_binary(&res).unwrap();
-        assert_eq!(expected, value);
-
-        let msg = ExecuteMsg::Sell {
-            id: "lily_id".to_string(),
-            amount: 45,
-        };
-        let info = mock_info("creator", &coins(1000, "earth"));
-        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-        // it worked, let's query the pen
-        let res = query(
-            deps.as_ref(),
-            mock_env(),
-            QueryMsg::GetPen {
-                id: "lily_id".to_string(),
-            },
-        )
-        .unwrap();
-        let flower = Pen {
-            id: "lily_id".to_string(),
-            name: "lily".to_string(),
-            amount: 55,
-            price: 100,
-        };
-        let expected = FlowerInfoResponse {
-            flower: Some(flower),
-        };
-        let value: FlowerInfoResponse = from_binary(&res).unwrap();
         assert_eq!(expected, value);
     }
